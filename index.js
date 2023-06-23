3d77a1aa6352eea15792f3d9126e393d9b36966a
Igor1D <43789550+Igor1D@users.noreply.github.com>
2023-06-23 12:41:15 +0200

    mobile + js ready

--- index.js
+++ index.js
@@ -2,21 +2,33 @@ let adviceP = document.getElementById('adviceText');
 let adviceNum = document.getElementById('adviceNum');
 let btn = document.getElementById('btn');
 
+
 async function getAdvice() {
     let results =  await fetch('https://api.adviceslip.com/advice');
-    let data = await results.json()
+    let data = await results.json();
     let adviceID = data.slip.id;
-    let adviceText = data.slip.advice
+    let adviceText = data.slip.advice;
+    let currentAdviceID = adviceNum.innerText.slice(1);
+
+    if (currentAdviceID == adviceID) {
+        console.log('same ID');
+        getAdvice();
+
+
+    } else {
+        adviceNum.innerText = '#' + adviceID;
+        adviceP.innerText = '"' + adviceText + '"';
+    }
     
-    // console.log(data.slip.id);
-    adviceNum.innerHTML = '#' + adviceID;
-    adviceP.innerHTML = '"' + adviceText + '"';
-
-    btn.addEventListener('click', () => {
-
-        
-    })
 
 }
 
-getAdvice()
+    btn.addEventListener('click', () => {
+
+        getAdvice();
+        
+    })
+
+getAdvice();
+
+
